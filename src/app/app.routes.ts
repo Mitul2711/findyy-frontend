df935f0074b1fc69ac34dd79f006bfbb82e4e5d8
Mitul <130025869+Mitul2711@users.noreply.github.com>
2025-10-06 10:47:00 +0000

    create a signup page design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 export const routes: Routes = [
-    {path: "", component: LoginComponent}
+    {path: "", component: LoginComponent},
+    {path: "signup", component: SignupComponent},
 ];
