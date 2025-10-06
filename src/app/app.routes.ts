39a6546bfdbcb7c229228248a399160a356976d7
Mitul <130025869+Mitul2711@users.noreply.github.com>
2025-10-06 13:06:49 +0000

    add home page design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
+import { HomeComponent } from './component/home/home.component';
 
 export const routes: Routes = [
-    {path: "", component: LoginComponent},
-    {path: "signup", component: SignupComponent},
+    {path: "", component: HomeComponent},
+    {path: "login", component: LoginComponent},
+    {path: "signup", component: SignupComponent}
 ];
