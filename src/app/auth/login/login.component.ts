f065715d47fc32ec4f758c2286424fe7851ff4ca
Mitul <130025869+Mitul2711@users.noreply.github.com>
2025-10-06 10:09:12 +0000

    add login design

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,11 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+email: string = '';
+  password: string = '';
 
+  onSubmit() {
+    console.log('Login attempt:', { email: this.email, password: this.password });
+    // Add your authentication logic here
+  }
 }
